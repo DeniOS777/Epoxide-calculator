c39813606d5090a4b0b0fc05d10a2e4fb9249961
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2022-01-26 22:41:02 +0200

    change files

--- src/index.js
+++ src/index.js
@@ -4,38 +4,60 @@ const refs = {
   input: document.querySelector('.input'),
   componentA: document.querySelector('.text__result-a'),
   componentB: document.querySelector('.text__result-b'),
-  button: document.querySelector('.btn-result'),
+  button: document.querySelector('.btn'),
 };
 
 refs.button.addEventListener('click', onButtonResultClick);
-
 refs.input.addEventListener('input', onEnterValue);
-
-refs.button.disabled = true;
 
 let inputValue = '';
 
 function onEnterValue(e) {
   inputValue = e.target.value.trim();
   if (!inputValue) {
-    refs.button.disabled = true;
-    refs.componentA.innerHTML = '';
-    refs.componentB.innerHTML = '';
+    clearRenderResults();
+    activatedAndVisibleButton();
     return;
   }
-  refs.button.disabled = false;
+  activatedAndVisibleButton();
 }
 
 function onButtonResultClick() {
+  clearRenderResults();
+
   if (!parseInt(inputValue)) {
     return console.log('Введите число');
   }
 
   const componentValueA = inputValue / 1.6;
+  renderComponentA(componentValueA);
+
   const componentValueB = inputValue - componentValueA;
-  const markupA = `${componentValueA} грамм`;
-  const markupB = `${componentValueB} грамм`;
-  refs.componentA.insertAdjacentHTML('beforeend', markupA);
-  refs.componentB.insertAdjacentHTML('beforeend', markupB);
+  renderComponentB(componentValueB);
+  disabledAndHiddenButton();
+}
+
+function clearRenderResults() {
+  refs.componentA.innerHTML = '';
+  refs.componentB.innerHTML = '';
+}
+
+function disabledAndHiddenButton() {
   refs.button.disabled = true;
+  refs.button.classList.add('is-hidden');
+}
+
+function activatedAndVisibleButton() {
+  refs.button.disabled = false;
+  refs.button.classList.remove('is-hidden');
+}
+
+function renderComponentA(componentValueA) {
+  const markupA = `${componentValueA} <span class="text--black">грамм</span>`;
+  refs.componentA.insertAdjacentHTML('beforeend', markupA);
+}
+
+function renderComponentB(componentValueB) {
+  const markupB = `${componentValueB} <span class="text--black">грамм</span>`;
+  refs.componentB.insertAdjacentHTML('beforeend', markupB);
 }
